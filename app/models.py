f9260e9d5001d9a300e0ef7c2e34001121b8b446
ikaros <1371689491@qq.com>
2017-04-23 20:43:25 +0800

    flask

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.Foreignkey('roles.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return '<User %s>' % self.username
